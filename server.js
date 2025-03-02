d387d5509267aacf3cd571f7658e46e588c6849b
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-03-02 10:59:47 -0300

    ok

--- server.js
+++ server.js
@@ -4,23 +4,44 @@
 
 // Rotas get = Listar , Post = Criar ,put = Editar varios , Patch = Editar Um  e delete = Deletar
 
+
 import express from 'express'   // importanndo a biblieteca express
+import { PrismaClient } from '@prisma/client'  // importanndo a biblieteca prisma
+
+const prisma = new PrismaClient()   // avriavel do prisma
+
+
 
 const app = express()
-app.use(express.json());
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', async (req, res) => {    // Rota salavando os usuarios
+
+  await prisma.user.create({
+    data: {
+      
+      email: req.body.email,    // mandando informaçoes para o banco 
+      name: req.body.name,
+      age: req.body.age
+
+    }
+
+    })
 
 
-app.get('/usuarios', (req, res)=> {
-    res.send('OLá, mundo! ');
+    res.status(201).json(req.body)
 })
 
-app.post('/usuarios', (req, res)=> {
-    res.send('Usuário criado ');
+
+app.get('/usuarios', (req, res)=> {    // Rota criando usuarios
+    res.status(200).json(users)
 })
+
+
 
 app.listen(3000)
-
-
 
 
 
@@ -36,10 +57,34 @@ app.listen(3000)
 // Deletar um usuário
 
 
+// Automático node --watch server.js
+
+
+// query params  :    servidor.com/usuarios?estado=bahia&cidade=salvador
+
+
+// Route Params  :   servidor.com/usuarios22   => informação especifica
+
+
+/* Bory Params {  
+
+"nome": "Anderson", "id":37
+
+
+} */
+
+// 200 -> SUCESSO
+// 400 -> ERRO CLIENTE
+// 500 -> ERRO SERVIDOR
+
+
+//   lignellidev    //  eelsZBVmapGgSiHR    MONGO DB
+
+// usando bioblioteca prisma fazer coneção com o banco de dados 
+
+// colocar o login e senha no env e depois do / colocar nome que dei no banco no caso USERS
 
 
 
 
-
-
-
+// npx prisma studio
